9568208686bc788d5472690d79426dd335128974
pimanzi <p.imanzi@alustudent.com>
2025-02-03 19:44:40 +0200

    styling the text

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      body: Center(
-        child: Text("Hello"),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          const Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 45, 7, 98)
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        child: Center(
+          child: Text(
+            "Hello World",
+            style: TextStyle(color: Colors.white, fontSize: 28.0),
+          ),
+        ),
       ),
     ),
   ));
