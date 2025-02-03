4e88b3b9a38694e5799abfc2cf366a0f1f6e1d31
pimanzi <p.imanzi@alustudent.com>
2025-02-03 23:04:37 +0200

    making reusable widgetss

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,16 +5,19 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(
+      {super.key, required this.color1, required this.color2});
+  final Color color1;
+  final Color color2;
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-        const Color.fromARGB(255, 26, 2, 80),
-        Color.fromARGB(255, 45, 7, 98)
-      ], begin: startAlignment, end: endAlignment)),
-      child: Center(child: StyledText()),
+          gradient: LinearGradient(
+              colors: [color1, color2],
+              begin: startAlignment,
+              end: endAlignment)),
+      child: Center(child: StyledText("Hello World")),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,10 @@ import 'package:rolldice_app/gradient_container.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(body: GradientContainer()),
+    home: Scaffold(
+        body: GradientContainer(
+      color1: Color.fromARGB(255, 26, 2, 80),
+      color2: Color.fromARGB(255, 45, 7, 98),
+    )),
   ));
 }
